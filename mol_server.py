899d5ff665dd062de414c81aa3f7c37d6d88da45
Guillermo Valle <guillefix@gmail.com>
2020-04-22 21:51:37 +0100

    added url for pdb and pubchem

--- mol_server.py
+++ mol_server.py
@@ -10,6 +10,10 @@ else:
     from io import StringIO
 import pandas as pd
 import re
+from convertmol import parse_sdf_file
+#%%
+pubchem_root = "pubchem_files"
+pdb_root = "pdb_files"
 
 # code="water"
 # code="5B27"
@@ -19,11 +23,11 @@ def atom_array_to_string(array):
     return ",".join(["["+";".join(["{:.4f}".format(c) for c in coords[:3]])+"]:"+str(coords[3])+":" for coords in array])+","
 
 def get_atoms_str_PDB(code):
-    if not os.path.exists(code+".pdb"):
-        urllib.request.urlretrieve("https://files.rcsb.org/download/"+code+".pdb", code+".pdb")
+    if not os.path.exists(pubchem_root+"/"+code+".pdb"):
+        urllib.request.urlretrieve("https://files.rcsb.org/download/"+code+".pdb", pubchem_root+"/"+code+".pdb")
     #parses PDB file to get the lines with the information about ATOMS
     atom_lines = []
-    with open(code+".pdb","r") as file:
+    with open(pubchem_root+"/"+code+".pdb","r") as file:
         for line in file.readlines():
             if line[:4] == "ATOM":
                 line = line[:4]+" "+line[30:]
@@ -43,16 +47,12 @@ def get_atoms_str_PDB(code):
     return atoms_str
 
 def get_atoms_str_PUBCHEM(name):
-    if not os.path.exists(name+".sdf"):
-        # try:
-            # urllib.request.urlretrieve("https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/CID/"+code+"/record/SDF/?record_type=3d&response_type=save&response_basename=Conformer3D_CID_"+code, code+".sdf")
-        # except:
-        #     urllib.request.urlretrieve("https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/CID/"+code+"/record/SDF/?record_type=2d&response_type=save&response_basename=Structure2D_CID_"+code, code+".sdf")
-        urllib.request.urlretrieve("https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/"+name+"/sdf",name+".sdf")
+    print(name)
+    if not os.path.exists(pubchem_root+"/"+name+".sdf"):
+        urllib.request.urlretrieve("https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/"+name+"/sdf",pubchem_root+"/"+name+".sdf")
 
-    from convertmol import parse_sdf_file
     # result = parse_sdf_file("Structure2D_CID_962.sdf")
-    result = parse_sdf_file(name+".sdf")
+    result = parse_sdf_file(pubchem_root+"/"+name+".sdf")
     atoms = filter(lambda x: x[0][0]=="?",result[0].items())
     atoms = map(lambda x:x[1],atoms)
     data = pd.DataFrame(atoms)
@@ -65,17 +65,18 @@ class S(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
+
     def do_GET(self):
         print(self.path)
         # if self.path == "/take":
+        if self.path == "/favicon.ico":
+            return
         self._set_headers()
-        # classification = open("classification.txt", "r").read()
-        # self.wfile.write(bytes("<html><body><h1>"+str(classification)+"</h1></body></html>", "utf-8"))
-        # poss = np.random.randn(10,3)
-        # self.wfile.write(bytes(str(",".join(["["+";".join([str(c) for c in coords])+"]" for coords in poss])), "utf-8"))
-        # self.wfile.write(bytes(str(atoms_str,"utf-8")))
-        # atoms_str = get_atoms_str_PDB(self.path[1:])
-        atoms_str = get_atoms_str_PUBCHEM(self.path[1:])
+        database, molecule_name = self.path.split("/")[1:]
+        if database == "pdb":
+            atoms_str = get_atoms_str_PDB(molecule_name)
+        if database == "pubchem":
+            atoms_str = get_atoms_str_PUBCHEM(molecule_name)
         self.wfile.write(bytes(atoms_str, "utf-8"))
 
     def do_HEAD(self):
